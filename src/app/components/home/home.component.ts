b305775a6372d54e7ead1443f34a7c8a1d91bbf1
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-08-27 19:32:13 -0400

    feat: Add CompanyComponent to home page
    The code changes add the CompanyComponent to the home page, including its HTML, TypeScript, and SCSS files. This component represents a company profile section and is intended to be used in the home page of the application.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { NoticeComponent } from '../notice/notice.component';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { ActionsCardComponent } from '../actions-card/actions-card.component';
+import { CompanyComponent } from './components/company/company.component';
+import { FinancialComponent } from "../actions-card/components/financial/financial.component";
 
-const components = [NoticeComponent, CarouselComponent,ActionsCardComponent];
+const components = [NoticeComponent, CarouselComponent,ActionsCardComponent, CompanyComponent];
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [components],
+  imports: [components, FinancialComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
