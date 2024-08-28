bfe09a5c0c2cf86a95e02462998d42ca12040ecd
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-08-28 19:05:38 -0400

    Actualizar componente de invitación con función de copiar al portapapeles

--- src/app/components/team/components/invitation/invitation.component.ts
+++ src/app/components/team/components/invitation/invitation.component.ts
@@ -10,4 +10,12 @@ import { Component } from '@angular/core';
 export class InvitationComponent {
   code = '703240';
   link = 'https://ai-redbullvip.com/#/register?ic=703240';
+
+  copyToClipboard(text: string): void {
+    navigator.clipboard.writeText(text).then(() => {
+      console.log('Texto copiado al portapapeles:', text);
+    }).catch(err => {
+      console.error('Error al copiar al portapapeles:', err);
+    });
+  }
 }
