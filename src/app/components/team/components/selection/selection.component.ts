2945fe2d628b8438e54500f4eed5839c35a0c369
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-08-28 19:46:03 -0400

    Actualizar componente de selección con imagen y texto

--- src/app/components/team/components/selection/selection.component.ts
+++ src/app/components/team/components/selection/selection.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './selection.component.html',
-  styleUrl: './selection.component.scss'
+  styleUrl: './selection.component.scss',
 })
 export class SelectionComponent {
-
+  teamSize = 0;
+  recharge = 0;
+  withdraw = 0;
+  newTeam = 0;
+  rechargeFirst = 0;
+  firstWithdraw = 0;
 }
