450ac1e2d910376c5a4e931eded96ddef99ad558
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-08-28 18:18:59 -0400

    Actualizar componentes de botones y tareas
    Los cambios en los componentes de botones y tareas actualizan el HTML para mejorar la usabilidad y el diseño. Esto incluye hacer que los botones sean más responsivos y agregar un nuevo componente de tareas para administrarlas.

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss',
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit{
   balance = '0.00';
   todayProfits = 0;
   profits = 0;
+  list=''
+  btnC=true
 
-  gafasVR() {}
+  ngOnInit(): void {
+    this.gafasVR()
+  }
 
-  myGafas() {}
+  gafasVR() {
+    this.list='lista gafas Vr'
+    this.btnC=true
+  }
+
+  myGafas() {
+    this.list='mi lista'
+    this.btnC=false
+  }
 }
