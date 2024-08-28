908291cb204f096a8c7b56701c5a5a1e7924dc92
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-08-28 15:57:38 -0400

    fix h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./components/home/components/nav/nav.component";
+import { ButtonsComponent } from "./components/buttons/buttons.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, ButtonsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
