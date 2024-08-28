5726d7014c38016b68148c0f75e283fef5ab7fa2
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-08-28 13:08:09 -0400

    time

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,14 +3,22 @@ import { NoticeComponent } from '../notice/notice.component';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { ActionsCardComponent } from '../actions-card/actions-card.component';
 import { CompanyComponent } from './components/company/company.component';
-import { FinancialComponent } from "../actions-card/components/financial/financial.component";
+import { TimeComponent } from './components/time/time.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
-const components = [NoticeComponent, CarouselComponent,ActionsCardComponent, CompanyComponent];
+const components = [
+  NoticeComponent,
+  CarouselComponent,
+  ActionsCardComponent,
+  CompanyComponent,
+  TimeComponent,
+  TasksComponent,
+];
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [components, FinancialComponent],
+  imports: [components],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
