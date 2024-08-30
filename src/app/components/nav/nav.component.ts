5141e88cc33565aa271885a8b7f3219d28b5c939
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-08-30 03:06:55 -0400

    feat: Update nav component structure and styles

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,29 @@
+import { NgClass } from '@angular/common';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss',
+})
+export class NavComponent implements OnInit {
+  isMeRoute = false;
+  isScrolled = false;
+
+  private router = inject(Router);
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isMeRoute = event.urlAfterRedirects === '/me';
+      }
+    });
+  }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.pageYOffset > 0;
+  }
+}
