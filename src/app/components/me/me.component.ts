05d902628327297e42cf2c53b17a9c2c09f8d4a6
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-08-30 04:21:45 -0400

    feat: Add "me" component UI and functionality

--- src/app/components/me/me.component.ts
+++ src/app/components/me/me.component.ts
@@ -5,8 +5,10 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './me.component.html',
-  styleUrl: './me.component.scss'
+  styleUrl: './me.component.scss',
 })
 export class MeComponent {
-
+  email = 'algo@mail.com';
+  monto = '0.00';
+  total = 0;
 }
