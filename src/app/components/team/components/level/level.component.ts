8e5c79e937ff18c73e83e335a9d9a8e3ae20c953
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-08-30 02:20:39 -0400

    Actualizar estilos del componente de selección de equipo

--- src/app/components/team/components/level/level.component.ts
+++ src/app/components/team/components/level/level.component.ts
@@ -5,8 +5,22 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './level.component.html',
-  styleUrl: './level.component.scss'
+  styleUrl: './level.component.scss',
 })
 export class LevelComponent {
-
+  lvl1 = {
+    register: 0,
+    commission: 0,
+    totalIncome: 0,
+  };
+  lvl2 = {
+    register: 0,
+    commission: 0,
+    totalIncome: 0,
+  };
+  lvl3 = {
+    register: 0,
+    commission: 0,
+    totalIncome: 0,
+  };
 }
