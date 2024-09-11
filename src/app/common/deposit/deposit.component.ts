667a2b073bc40da1088e1023cfa12dcb78476c5b
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-09-11 11:12:26 -0700

    Add deposit component and related files

--- src/app/common/deposit/deposit.component.ts
+++ src/app/common/deposit/deposit.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BackHomeComponent } from '../../shared/back-home/back-home.component';
+import { QrComponent } from './qr/qr.component';
 
 @Component({
   selector: 'app-deposit',
   standalone: true,
-  imports: [],
+  imports: [BackHomeComponent,QrComponent],
   templateUrl: './deposit.component.html',
   styleUrl: './deposit.component.scss'
 })
