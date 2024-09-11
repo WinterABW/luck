460b14307408ed040ad7a555c08ae22964bc9eea
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-09-11 11:47:48 -0700

    Refactor deposit component HTML: Update address layout and styling

--- src/app/common/deposit/deposit.component.ts
+++ src/app/common/deposit/deposit.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { BackHomeComponent } from '../../shared/back-home/back-home.component';
 import { QrComponent } from './qr/qr.component';
+import { AddressComponent } from './address/address.component';
+import { InfoComponent } from './info/info.component';
 
 @Component({
   selector: 'app-deposit',
   standalone: true,
-  imports: [BackHomeComponent,QrComponent],
+  imports: [BackHomeComponent, QrComponent, AddressComponent, InfoComponent],
   templateUrl: './deposit.component.html',
-  styleUrl: './deposit.component.scss'
+  styleUrl: './deposit.component.scss',
 })
-export class DepositComponent {
-
-}
+export class DepositComponent {}
