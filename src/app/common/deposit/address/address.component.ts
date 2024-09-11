73e5d613713953127154130ca56dc3ce9188a9cd
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-09-11 15:23:06 -0700

    udpate

--- src/app/common/deposit/address/address.component.ts
+++ src/app/common/deposit/address/address.component.ts
@@ -5,8 +5,21 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './address.component.html',
-  styleUrl: './address.component.scss'
+  styleUrl: './address.component.scss',
 })
 export class AddressComponent {
-
+  link: string = 'TThr4nZA59XZ5MC8ZPkUdjPa7QAtpAx692';
+  copyToClipboard(text: string, button: HTMLButtonElement): void {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        button.textContent = 'Copied';
+        setTimeout(() => {
+          button.textContent = 'Copiar';
+        }, 1000);
+      })
+      .catch((err) => {
+        console.error('Error al copiar al portapapeles:', err);
+      });
+  }
 }
