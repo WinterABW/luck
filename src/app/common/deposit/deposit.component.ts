c31c92acac176f336ea6fa739fc7d5b17f1fd148
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-09-15 15:29:37 -0700

    Actualizar estilos de tarjeta y rejilla en la página de gafas

--- src/app/common/deposit/deposit.component.ts
+++ src/app/common/deposit/deposit.component.ts
@@ -3,15 +3,21 @@ import { BackHomeComponent } from '../../shared/back-home/back-home.component';
 import { QrComponent } from './qr/qr.component';
 import { AddressComponent } from './address/address.component';
 import { InfoComponent } from './info/info.component';
+import { NequiComponent } from './nequi/nequi.component';
 
 @Component({
   selector: 'app-deposit',
   standalone: true,
-  imports: [BackHomeComponent, QrComponent, AddressComponent, InfoComponent],
+  imports: [
+    BackHomeComponent,
+    QrComponent,
+    AddressComponent,
+    InfoComponent,
+    NequiComponent,
+  ],
   templateUrl: './deposit.component.html',
   styleUrl: './deposit.component.scss',
 })
 export class DepositComponent {
   @Input('token') token: string = '';
-  
 }
